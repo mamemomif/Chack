03a79d26033add30a55ad9762c838da48901ff3d
Youngjun Hong <moeji@hansung.ac.kr>
2024-11-07 22:47:34 +0900

    feat: implements bottom navigation bar

---
+++ lib/components/custom_bottom_nav_bar.dart
@@ -0,0 +1,98 @@
+// lib/components/bottom_navigation/custom_bottom_navigation_bar.dart
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import '../constants/colors.dart';
+import '../constants/icons.dart';
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const CustomBottomNavigationBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 10,
+            offset: const Offset(0, -5),
+          ),
+        ],
+      ),
+      child: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          _buildNavigationItem(
+            icon: AppIcons.homeIcon,
+            label: '홈',
+          ),
+          _buildNavigationItem(
+            icon: AppIcons.bookShelfIcon,
+            label: '서재',
+          ),
+          _buildNavigationItem(
+            icon: AppIcons.timerIcon,
+            label: '타이머',
+          ),
+          _buildNavigationItem(
+            icon: AppIcons.statisticsIcon,
+            label: '통계',
+          ),
+        ],
+        currentIndex: currentIndex,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: AppColors.backgroundColor,
+        selectedItemColor: AppColors.pointColor,
+        unselectedItemColor: AppColors.subTextColor,
+        selectedFontSize: 11,
+        unselectedFontSize: 11,
+        selectedLabelStyle: const TextStyle(
+          fontFamily: 'SUITE',
+          fontWeight: FontWeight.w600, // SemiBold
+          height: 1.5,
+        ),
+        unselectedLabelStyle: const TextStyle(
+          fontFamily: 'SUITE',
+          fontWeight: FontWeight.w400, // Regular
+          height: 1.5,
+        ),
+        onTap: onTap,
+      ),
+    );
+  }
+
+  BottomNavigationBarItem _buildNavigationItem({
+    required String icon,
+    required String label,
+  }) {
+    return BottomNavigationBarItem(
+      icon: Padding(
+        padding: const EdgeInsets.only(bottom: 4),
+        child: SvgPicture.asset(
+          icon,
+          colorFilter: ColorFilter.mode(
+            AppColors.subTextColor,
+            BlendMode.srcIn,
+          ),
+        ),
+      ),
+      activeIcon: Padding(
+        padding: const EdgeInsets.only(bottom: 4),
+        child: SvgPicture.asset(
+          icon,
+          colorFilter: ColorFilter.mode(
+            AppColors.pointColor,
+            BlendMode.srcIn,
+          ),
+        ),
+      ),
+      label: label,
+    );
+  }
+}
